f2ef376c2c10a4a244e4000689635f7d8b2eccd3
kala8 <30865966+kala8@users.noreply.github.com>
2017-08-10 20:48:57 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,11 +22,17 @@ var articleOne= {
          </p>`
 };
 
+function createTemplate (data) {
+var title = data.title;
+var date =data.date;
+var heading =data.heading;
+var content =data.content;
+
 var htmlTemplate = `
 <html>
   <head>
       <title>
-         Article One : Kalyani.A
+        ${title}
       </title>
       <meta name="viewport" content="width-device-width, initial-scale-1/">
         <link href="/ui/style.css" rel="stylesheet" />
@@ -38,24 +44,27 @@ var htmlTemplate = `
      </div>
      <hr/>
      <h3>
-         Article One
+         ${heading}
      </h3>
      <div>
-         August 9, 2017
+         ${date}
      </div>
      <div>
-       $(content)
+       ${content}
       </div>     
     </div>
  </body>   
 </html>
 `;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleOne))
 });
 
 app.get('/article-two', function(req,res){
