5f6268bedf16b5722f84ba652113a5327b037818
kala8 <30865966+kala8@users.noreply.github.com>
2017-08-11 20:06:50 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,18 @@ app.get('/counter', function (req, res){
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req,res){
+    //get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON: javascript object notation
+
+    res.send(JSON.stringify(names));
+    
+});
+
 app.get('/article-one', function(req,res){
      res.send(createTemplate(articleOne))
 });
@@ -92,18 +104,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name', function (req,res){
-    //get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    // JSON: javascript object notation
-
-    res.send(JSON.stringify(names));
-    
 });
 
 
