6ecc42f6e916889c20168d0df128691058f6d92d
kala8 <30865966+kala8@users.noreply.github.com>
2017-08-11 20:04:42 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,9 +95,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req,res){
+app.get('/submit-name', function (req,res){
     //get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     // JSON: javascript object notation
